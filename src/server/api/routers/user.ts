191e06a8316ed250458fef0974f2c2bec1615629
Ssam <s.j.millington@hotmail.co.uk>
2023-09-07 19:17:44 +0100

    working

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -5,6 +5,8 @@ import {
   protectedProcedure,
 } from "~/server/api/trpc";
 
+import openai from "~/server/openai/client"
+
 export const userRouter = createTRPCRouter({
     update: protectedProcedure
         .input(z.object({ data: z.object({
@@ -33,5 +35,67 @@ export const userRouter = createTRPCRouter({
                     id: ctx.session.user.id
                 }
             })
+        }),
+    personalSection: protectedProcedure
+        .input(z.object({ data: z.object({
+            
+            prompt: z.string(),
+            returnGPT: z.boolean()
+
+        }) }))
+        .mutation(async ({ ctx, input }) => {
+
+            const { prompt, returnGPT } = input.data
+
+            const user = await ctx.prisma.user.findFirst({
+                include: {
+                    personal: true
+                },
+                where: {
+                    id: ctx.session.user.id
+                }
+            })
+
+            if(user === null) {
+                throw new Error('No user?!')
+            }
+
+            const completion = await openai.chat.completions.create({
+                messages: [
+                    {
+                        role: 'system',
+                        content: 'Generate a single paragraph in the third person in the style of a personal section of a professional curriculum vitae'
+                    },
+                    {
+                        role: 'user',
+                        content: prompt
+                    }
+                ],
+                model: 'gpt-3.5-turbo'
+            });
+
+            const result = completion.choices[0]?.message.content ?? 'Unable to generate anything from this. Please try again.'
+
+            if(!user.personal) {
+                await ctx.prisma.personalEntry.create({
+                    data: {
+                        userId: user.id,
+                        prompt,
+                        result
+                    }
+                })
+            } else {
+                await ctx.prisma.personalEntry.update({
+                    where: {
+                        id: user.personal.id
+                    },
+                    data: {
+                        prompt,
+                        result
+                    }
+                })
+            }
+
+            return result
         })
 });
